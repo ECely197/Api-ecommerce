f367c1ee7151ab813579254a45646f576f3a92d8
Edwin Cely <edwincely6@gmail.com>
2024-12-17 20:58:04 -0600

    order tests

--- __tests__/auth.controller.test.js
+++
@@ -1,6 +0,0 @@
-import {beforeEach, describe, expect, jest} from '@jest/globals';
-import { request } from 'supertest';
-
-beforeEach(() =>{
-    jest.unstable_mockModule
-})
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,6 @@
 import Order from "../models/Order.js";
 
-async function getAllOrders(req, res) {
+export const getAllOrders = async (req, res) => {
   try {
     const orders = await Order.find({ deleteAt: null });
     return res.status(200).json(orders);
@@ -9,9 +9,9 @@ async function getAllOrders(req, res) {
       .status(500)
       .json({ message: `Error al buscar todos los pedidos`, error });
   }
-}
+};
 
-async function getORderById(req, res) {
+export const getOrderById = async (req, res) => {
   try {
     const orderId = await Order.findById(req.params.id);
     if (!orderId || orderId.deleteAt) {
@@ -23,61 +23,51 @@ async function getORderById(req, res) {
       .status(500)
       .json({ message: "Error al buscar el pedido", error });
   }
-}
+};
 
-async function createOrder(req, res) {
+export const createOrder = async (req, res) => {
   const { user, products, totalAmount, status } = req.body;
   try {
-    const newOrder = new Order({
-      user,
-      products,
-      totalAmount,
-      status,
-    });
+    const newOrder = new Order({ user, products, totalAmount, status });
     await newOrder.save();
     return res.status(201).json(newOrder);
   } catch (error) {
     return res.status(500).json({ message: "Error al crear el pedido", error });
   }
-}
+};
 
-async function updateOrder(req, res) {
-  const updateOrder = await Order.findById(req.params.id);
+
+export const updateOrder = async (req, res) => {
   try {
-    if (updateOrder !== null) {
+    const updateOrder = await Order.findById(req.params.id);
+    if (updateOrder) {
       const { products, totalAmount, status } = req.body;
       updateOrder.products = products || updateOrder.products;
       updateOrder.totalAmount = totalAmount || updateOrder.totalAmount;
       updateOrder.status = status || updateOrder.status;
-
       await updateOrder.save();
-
       return res.status(200).json("El pedido ha sido actualizado");
+    } else {
+      return res.status(404).json({ message: 'Pedido no encontrado' });
     }
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Error al actualizar el pedido", error });
+    return res.status(500).json({ message: "Error al actualizar el pedido", error });
   }
-}
-async function softDeleteOrder(req, res) {
-  const orderToDelete = await Order.findById(req.params.id);
-
-  try {
-    orderToDelete.deleteAt = Date.now();
-    orderToDelete.save();
-    return res.status(200).json("El pedido ha sido elminado");
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Error al eliminar el pedido", error });
-  }
-}
-
-export default {
-  getAllOrders: getAllOrders,
-  getORderById: getORderById,
-  createOrder: createOrder,
-  updateOrder: updateOrder,
-  softDeleteOrder: softDeleteOrder,
 };
+
+
+export const softDeleteOrder = async (req, res) => {
+  try {
+    const orderToDelete = await Order.findById(req.params.id);
+    if (orderToDelete) {
+      orderToDelete.deleteAt = Date.now();
+      await orderToDelete.save();
+      return res.status(200).json("El pedido ha sido elminado");
+    } else {
+      return res.status(404).json({ message: 'Pedido no encontrado' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Error al eliminar el pedido", error });
+  }
+};
+
