216ea9503521b6dd7f37d5062ac6357facceca6c
Edwin Cely <edwincely6@gmail.com>
2024-12-17 21:54:56 -0600

    error routes order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import orderController from "../controllers/orderController.js";
+import { getAllOrders, getOrderById, createOrder, updateOrder, softDeleteOrder } from "../controllers/orderController.js";
 
 const router = express.Router();
 
-router.get("/api/order", orderController.getAllOrders);
-router.get("/api/order/:id", orderController.getORderById);
-router.post("/api/order", orderController.createOrder);
-router.patch("/api/order/:id", orderController.updateOrder);
-router.delete("/api/order/:id", orderController.softDeleteOrder);
+router.get("/api/order", getAllOrders);
+router.get("/api/order/:id", getOrderById);
+router.post("/api/order", createOrder);
+router.patch("/api/order/:id", updateOrder);
+router.delete("/api/order/:id", softDeleteOrder);
 
 export default router;
